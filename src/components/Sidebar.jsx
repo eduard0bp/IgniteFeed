1d2fe222558d65122a014e4e30b3f685ffa6de0e
Eduardo Batista Pereira <duuhpwn@gmail.com>
2023-01-23 20:32:15 -0300

    fix: finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,25 @@
+import { PencilLine } from 'phosphor-react'
 import './Sidebar.scss'
 
 export const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <img
-        src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-      />
+      <img src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
-      <div className='profile'>
-        <strong>Username</strong>
-        <span>Cargo</span>
+      <div className="profile">
+        <img
+          src="https://avatars.githubusercontent.com/u/79476975?v=4"
+          alt=""
+        />
+        <strong>Eduardo Batista</strong>
+        <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
